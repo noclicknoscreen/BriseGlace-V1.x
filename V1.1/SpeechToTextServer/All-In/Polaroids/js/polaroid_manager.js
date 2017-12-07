38eda4c3cdf38688f14a4aeac1a707487fa92dc5
Matthias RIGAUD <matthias.rigaud@epitech.eu>
2017-12-07 14:30:40 +0100

    back to the future

--- V1.1/SpeechToTextServer/All-In/Polaroids/js/polaroid_manager.js
+++ V1.1/SpeechToTextServer/All-In/Polaroids/js/polaroid_manager.js
@@ -3,64 +3,95 @@ var puzzleIndex = -1
 var cluesIndex = 0
 var fileList = []
 var currentPuzzle = null
-var maxImgSize = 400;
+var maxImgSize = 300;
+var amplitudeAngle = 40.0;
 
 function getCurrentResponse()
 {
-  return (currentPuzzle.wordToFind);
+    return (currentPuzzle.wordToFind);
 }
 
 function popPolaroid()
 {
-  var img = currentPuzzle.clues[cluesIndex]
-  cluesIndex++;
-  var newLi = document.createElement("li");
-  var newA = document.createElement("a");
-  var newImg = document.createElement("img");
-  newImg.src = img.picture;
-  newImg.onload = function() {
-    var height = this.naturalHeight;
-    var width = this.naturalWidth;
+    var img = currentPuzzle.clues[cluesIndex]
+    cluesIndex++;
+    var newLi = document.createElement("li");
+    var newA = document.createElement("a");
+    var newImg = document.createElement("img");
+    newA.href = "";
+    newA.title = img.keyWord;
+    newA.appendChild(newImg);
+    newLi.appendChild(newA);
+    newImg.onload = function() {
+	var height = this.naturalHeight;
+	var width = this.naturalWidth;
 
-    if (height > width && height > maxImgSize) {
-      this.style.width = "auto";
-      this.style.height = maxImgSize.toString() + "px";
+	if (height > width && height > maxImgSize) {
+	    this.style.width = "auto";
+	    this.style.height = maxImgSize.toString() + "px";
+	}
+	else if (width > maxImgSize) {
+	    this.style.height = "auto";
+	    this.style.width = maxImgSize.toString() + "px";
+	}
+	var angle = (Math.random() * amplitudeAngle) - (amplitudeAngle / 2);
+	newLi.style.transform = "rotate(" + angle.toString() + "deg)";
+	newLi.style["-moz-transform"] = "rotate(" + angle.toString() + "deg)";
+	newLi.style["-webkit-transform"] = "rotate(" + angle.toString() + "deg)";
+	newLi.style.position = "fixed";
+	newLi.style.left = "" + Math.random() * 70 + 15 + "%";
+	newLi.style.top = "" + Math.random() * 15 + "%";
+	document.getElementById("polaroids").appendChild(newLi);
     }
-    else if (width > maxImgSize) {
-      this.style.height = "auto";
-      this.style.width = maxImgSize.toString() + "px";
+    newImg.src = img.picture;
+    if (cluesIndex >= currentPuzzle.clues.length - 1)
+	return false;
+    return true;
+}
+
+function preloadImages(puzzle) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
     }
-  }
-  newA.href = "";
-  newA.title = img.keyWord;
-  newA.appendChild(newImg);
-  newLi.appendChild(newA);
-  document.getElementById("polaroids").appendChild(newLi);
-  if (cluesIndex >= currentPuzzle.clues.length)
-  return false;
-  return true;
+    var list = preloadImages.list;
+    for (var i = 0; i < puzzle.clues.length; i++) {
+        var img_t = new Image();
+        img_t.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                list.splice(index, 1);
+            }
+        }
+        list.push(img_t);
+        img_t.src = puzzle.clues[i].picture;
+    }
 }
 
 function launchNewPuzzle() {
-  puzzleIndex++;
+    if (!fileList.length)
+	return ;
+    document.getElementById("polaroids").innerHTML = "";
+    cluesIndex = 0;
+    puzzleIndex++;
 
-  if (puzzleIndex >= fileList.length)
-  puzzleIndex = 0;
+    if (puzzleIndex >= fileList.length)
+	puzzleIndex = 0;
 
-  $.ajax({
-    type: "GET",
-    url: addr + fileList[puzzleIndex],
-    dataType: "json",
-    success: function(response) {
-      currentPuzzle = response;
-    }});
-  }
+    $.ajax({
+	type: "GET",
+	url: addr + fileList[puzzleIndex],
+	dataType: "json",
+	success: function(response) {
+	    preloadImages(response);
+	    currentPuzzle = response;
+	}});
+}
 
-  $.ajax({
+$.ajax({
     type: "GET",
     url: addr + "index.json",
     dataType: "json",
     success: function(response) {
-      fileList = response.data;
-      launchNewPuzzle();
+	fileList = response.data;
+	launchNewPuzzle();
     }});
