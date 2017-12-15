01e12ff353ad8e64ba9692b2af9db1e2c30fc67d
Matthias RIGAUD <matthias.rigaud@epitech.eu>
2017-12-15 18:32:42 +0100

    back to the future of the past

---
+++ V1.1/SpeechToTextServer/_publicP5_V2/polaroid_manager.js
@@ -0,0 +1,128 @@
+var addr="http://localhost:3000/PuzzleSamples/";
+var puzzleIndex = -1;
+var cluesIndex = 0;
+var coordList = [];
+var fileList = [];
+var currentPuzzle = null;
+var maxImgSize = 300;
+var amplitudeAngle = 40.0;
+var minDistPol = 50;
+
+function getCurrentResponse()
+{
+    return (currentPuzzle.wordToFind);
+}
+
+function popPolaroid()
+{
+    var img = currentPuzzle.clues[cluesIndex]
+    cluesIndex++;
+    var newLi = document.createElement("li");
+    var newDiv = document.createElement("div");
+    var newImg = document.createElement("img");
+    var newMask = document.createElement("img");
+    newDiv.href = "";
+    newMask.style.position = "absolute";
+    newMask.style.top = "0%";
+    newMask.style.left = "0%";
+    newMask.src = "./Images/polaroid-mask.png";
+    newDiv.innerHtml = img.keyWord;
+    newDiv.style.overflow = "hidden";
+    newDiv.style.height = "" + newMask.naturalHeight + "px";
+    newDiv.style.width = "" + newMask.naturalWidth + "px";
+    newDiv.style["border-radius"] = "3px";
+    newMask.style["border-radius"] = "3px";
+    newDiv.appendChild(newMask);
+    newDiv.appendChild(newImg);
+    newLi.appendChild(newDiv);
+    newImg.onload = function() {
+	var height = this.naturalHeight;
+	var width = this.naturalWidth;
+
+	if (height > width && height > maxImgSize) {
+	    this.style.width = "auto";
+	    this.style.height = maxImgSize.toString() + "px";
+	}
+	else if (width > maxImgSize) {
+	    this.style.height = "auto";
+	    this.style.width = maxImgSize.toString() + "px";
+	}
+	var angle = (Math.random() * amplitudeAngle) - (amplitudeAngle / 2);
+	newLi.style.transform = "rotate(" + angle.toString() + "deg)";
+	newLi.style["-moz-transform"] = "rotate(" + angle.toString() + "deg)";
+	newLi.style["-webkit-transform"] = "rotate(" + angle.toString() + "deg)";
+	newLi.style.position = "fixed";
+	var haveGoodCoord = false;
+	var coord = null;
+	while (!haveGoodCoord) {
+	    coord = {
+		top: Math.random() * 15,
+		left: Math.random() * 70 + 15
+	    };
+	    if (coordList.length == 0)
+		break;
+	    for (var c of coordList) {
+		haveGoodCoord = false;
+		if (Math.pow(c.top - coord.top, 2) + Math.pow(c.left - coord.left, 2) < minDistPol)
+		    break;
+		haveGoodCoord = true;
+	    }
+	}
+	newLi.style.left = "" + coord.left + "%";
+	newLi.style.top = "" + coord.top + "%";
+	coordList.push(coord);
+	document.getElementById("polaroids").appendChild(newLi);
+    }
+    newImg.src = img.picture;
+    if (cluesIndex >= currentPuzzle.clues.length - 1)
+	return false;
+    return true;
+}
+
+function preloadImages(puzzle) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < puzzle.clues.length; i++) {
+        var img_t = new Image();
+        img_t.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                list.splice(index, 1);
+            }
+        }
+        list.push(img_t);
+        img_t.src = puzzle.clues[i].picture;
+    }
+}
+
+function launchNewPuzzle() {
+    if (!fileList.length)
+	return ;
+    coordList = [];
+    document.getElementById("polaroids").innerHTML = "";
+    cluesIndex = 0;
+    puzzleIndex++;
+
+    if (puzzleIndex >= fileList.length)
+	puzzleIndex = 0;
+
+    $.ajax({
+	type: "GET",
+	url: addr + fileList[puzzleIndex],
+	dataType: "json",
+	success: function(response) {
+	    preloadImages(response);
+	    currentPuzzle = response;
+	}});
+}
+
+$.ajax({
+    type: "GET",
+    url: addr + "index.json",
+    dataType: "json",
+    success: function(response) {
+	fileList = response.data;
+	launchNewPuzzle();
+    }});
--- V1.1/SpeechToTextServer/_publicP5_V2/sketch.js
+++ V1.1/SpeechToTextServer/_publicP5_V2/sketch.js
@@ -16,20 +16,20 @@ var index;
 function preload() {
 
   // Gallery ----------------------------------------------------------
-  myGallery = new gallery();
-  myGallery.preload();
+  // myGallery = new gallery();
+  // myGallery.preload();
 
 }
 
 function setup() {
   // Chargement des liens
-  myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/8/81/Brigniais.jpg");
-  myGallery.addLink("bataille", "https://c2.staticflickr.com/4/3941/15449767249_6d784e3c61_b.jpg");
-  myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/8/8e/Brignais_-_Aqueduc_du_Gier_-1.JPG");
-  myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/0/0c/SF_859_-_BRIGNAIS_-_La_Place.JPG");
-  myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/4/43/Blason_ville_fr_Brignais_%28Rh%C3%B4ne%29.svg");
-  myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/9/92/Brignais_vue_du_pont_vieux.jpg");
-  myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/1/1b/Brignais_Garon.jpg");
+  // myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/8/81/Brigniais.jpg");
+  // myGallery.addLink("bataille", "https://c2.staticflickr.com/4/3941/15449767249_6d784e3c61_b.jpg");
+  // myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/8/8e/Brignais_-_Aqueduc_du_Gier_-1.JPG");
+  // myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/0/0c/SF_859_-_BRIGNAIS_-_La_Place.JPG");
+  // myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/4/43/Blason_ville_fr_Brignais_%28Rh%C3%B4ne%29.svg");
+  // myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/9/92/Brignais_vue_du_pont_vieux.jpg");
+  // myGallery.addLink("bataille", "https://upload.wikimedia.org/wikipedia/commons/1/1b/Brignais_Garon.jpg");
 
 }
 
